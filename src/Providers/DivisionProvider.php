87e57056365114f074f8ea98ea0ffbe82996ef66
alirezakohandani <alireza.ko73@gmail.com>
2023-03-29 17:51:00 +0330

    initial commit

---
+++ src/Providers/DivisionProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Alirezakohandani\Division\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class DivisionProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
